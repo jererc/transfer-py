9286b44196be5a630b4ddc81a2bc8f315933f13f
Jerome Clerc <jererc@gmail.com>
2013-01-04 20:28:17 +0100

    clean static

--- transfer/webui/static/js/transfers.js
+++ transfer/webui/static/js/transfers.js
@@ -2,29 +2,29 @@ function initAddOverlay() {
     $('#overlay_trigger').overlay({
         mask: 'black',
         top: 'center',
-        });
-    };
+    });
+};
 
 function initActions() {
     $('.content_element').mouseover(function() {
         $(this).addClass('element_highlight');
         $(this).find('.element_actions').show();
-        });
+    });
     $('.content_element').mouseleave(function() {
         $(this).removeClass('element_highlight');
         $(this).find('.element_actions').hide();
-        });
+    });
 
     $('.img_button[alt="add"]').mouseover(function() {
         var content = $(this).parents('.content_new')[0];
         $(content).addClass('element_highlight', 200);
         $(content).find('.element_new').slideDown('fast');
-        });
+    });
     $('.content_new').mouseleave(function() {
         $(this).find('.element_new').slideUp('slow', function() {
             $('.content_new').removeClass('element_highlight', 200);
-            });
         });
+    });
 
     $('.img_button[alt="add"]').bind('click', function() {
         var div = $(this).parents('.content_new')[0];
@@ -34,19 +34,18 @@ function initActions() {
                 if (data.message) {
                     $('.add_message').text(data.message);
                     $('#overlay_trigger').overlay().load();
-                    }
-                else {
+                } else {
                     location.reload();
-                    }
-                });
+                }
+            });
         return false;
-        });
+    });
 
     $('.img_button[alt="more"]').bind('click', function() {
         var div = $(this).parents('.content_element')[0];
         $(div).find('.element_info').slideToggle('fast');
         return false;
-        });
+    });
 
     $('.img_button[alt="remove"]').bind('click', function() {
         var div = $(this).parents('.content_element')[0];
@@ -55,48 +54,46 @@ function initActions() {
             function(data) {
                 if (data.result == true) {
                     $(div).fadeOut();
-                    }
-                });
+                }
+            });
         return false;
-        });
-    };
+    });
+};
 
 function updateTransfer() {
-    if (has_focus) {
-        $('.content_element').each( function(result) {
+    if (hasFocus) {
+        $('.content_element').each(function(result) {
             var download = $(this);
-            var progressbar = $(this).find('.progressbar');
-            var content_info = $(this).find('.element_info');
+            var progressBar = $(this).find('.progressbar');
+            var contentInfo = $(this).find('.element_info');
 
             $.getJSON($SCRIPT_ROOT + '/transfers/update',
                 {id: $(this).find('input[name="id"]').val()},
                 function(data) {
                     if (data.name == null) {
                         download.fadeOut();
-                        }
-                    else {
+                    } else {
                         download.find('.name').html(data.name);
-                        content_info.find('.progress').html(data.progress);
-                        content_info.find('.transferred').html(data.transferred);
-                        content_info.find('.size').html(data.size);
+                        contentInfo.find('.progress').html(data.progress);
+                        contentInfo.find('.transferred').html(data.transferred);
+                        contentInfo.find('.size').html(data.size);
 
                         if (data.progress > 0) {
                             var width_total = download.find('.progress').width();
-                            progressbar.width(parseInt(data.progress * width_total / 100));
-                            progressbar.show();
-                            }
-                        else {
-                            progressbar.hide();
-                            }
+                            progressBar.width(parseInt(data.progress * width_total / 100));
+                            progressBar.show();
+                        } else {
+                            progressBar.hide();
                         }
-                    });
-            });
-        }
-    };
+                    }
+                });
+        });
+    }
+};
 
 $(function() {
     initAddOverlay();
     initActions();
     updateTransfer();
-    var progress_interval = window.setInterval(updateTransfer, 5000);
-    });
+    var progressInterval = window.setInterval(updateTransfer, 5000);
+});
